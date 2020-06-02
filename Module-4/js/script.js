12016aa60e048eca61f4ed001bf48bc556fa65ba
Bennie Draaijer <bennie007@live.nl>
2020-06-02 18:30:44 +0200

    Winc Academy | Eindopdracht Module 4
    Versie: kopie van origineel ingeleveverd eindproject (02.06.2020) Module 4. Original DUO Repo: https://github.com/taipo1/Hangman_Bennie

---
+++ Module-4/js/script.js
@@ -0,0 +1,172 @@
+const wordList = [
+  "vis",
+  // "toeter",
+  // "developer",
+  // "telefoon",
+  // "moeder",
+  // "snoer",
+  // "geeuw",
+];
+let inputs;
+let word;
+let gameOver;
+let tries = 0;
+let givenValue;
+const functions = {
+  assignWinningWord: (list) => {
+    const index = Math.floor(Math.random() * list.length);
+    word = list[index].split("");
+    return list[index].split("");
+  },
+
+  showLoseScreen: () => {
+    animateFunctions.hideInputAndButtons();
+    document.querySelector(".lose").style.display = "block";
+    gameOver = true;
+  },
+
+  showWinningScreen: () => {
+    animateFunctions.hideInputAndButtons();
+    document.querySelector(".win").style.display = "block";
+    gameOver = true;
+  },
+
+  // validates the input to be true or false
+  checkInputValue: (word, inputs) => {
+    let isTrue = false;
+    if (word.includes(inputs)) {
+      isTrue = true;
+    }
+    return isTrue;
+  },
+
+  // fetches input value
+  fetchValue: () => {
+    givenValue = document.querySelector("input").value;
+    document.querySelector("input").value = "";
+    return givenValue;
+  },
+
+  // updates the dom with wrong inputs
+  updateDomWrongInput: () => {
+    document.querySelector(".lives span").innerHTML = 5 - tries;
+    animateFunctions.animateHangMan(tries);
+  },
+
+  // calls the right functions for false or true statements
+  inputValidator: (input, word) => {
+    const validatedValue = f.checkInputValue(word, input);
+    if (validatedValue) {
+      return;
+    } else {
+      tries++;
+      return false;
+    }
+  },
+
+  filterGuessedLetters: (word, inputs) => {
+    const wrongLetters = inputs.filter((letter) => {
+      return !word.includes(letter);
+    });
+    // Comment out for jest.
+    document.querySelector(".guessed_letters").innerHTML = wrongLetters.join(
+      " "
+    );
+    return wrongLetters;
+  },
+
+  checkWinCondition: (word, inputs) => {
+    let remaining = word.filter((letter) => {
+      return !inputs.includes(letter);
+    });
+    // if the array is empty return true
+    if (remaining.length === 0) {
+      f.showWinningScreen(); // comment out for jest
+      gameOver = true;
+      return true;
+    } else {
+      return false;
+    }
+  },
+  checkLoseCondition: (tries) => {
+    if (tries === 5) {
+      f.showLoseScreen(); // comment out for jest
+      gameOver = true;
+      return true;
+    }
+  },
+
+  hideGameScreen: () => {
+    document.querySelector(".win").style.display = "none";
+    document.querySelector(".lose").style.display = "none";
+  },
+};
+const f = functions;
+
+const UpdateDomWinningWord = (word, inputLetterWords) => {
+  const display = word.map((letter) => {
+    if (inputLetterWords.includes(letter)) {
+      return letter;
+    } else {
+      return "_";
+    }
+  });
+  document.querySelector(".the_word").innerHTML = display.join(" ");
+};
+
+//Remove letters that are in the winning word from the array . push left over to dom
+const filterGuessedLetters = (word, inputs) => {
+  const wrongLetters = inputs.filter((letter) => {
+    return !word.includes(letter);
+  });
+  document.querySelector(".guessed_letters").innerHTML = wrongLetters.join(" ");
+};
+
+// Input handeler Master function
+const guessLetter = () => {
+  if (gameOver) {
+    return;
+  }
+  const inputValue = f.fetchValue();
+  // Check if value is true/false to winning word
+  f.inputValidator(inputValue, word);
+  // push value to inputs array
+  inputs.push(inputValue);
+
+  // Dom updates
+  f.updateDomWrongInput();
+  UpdateDomWinningWord(word, inputs);
+
+  filterGuessedLetters(word, inputs);
+  // check for game win/lose condition
+  f.checkWinCondition(word, inputs);
+  f.checkLoseCondition(tries);
+};
+
+// Game functionality
+
+const startGame = () => {
+  //////////////////////////////////////
+  animateFunctions.resetHangMan();
+  animateFunctions.showInputAndButtons();
+  //////////////////////////////////////
+  gameOver = false;
+  inputs = [];
+  tries = 0;
+  f.assignWinningWord(wordList);
+  document.querySelector("input").value = "";
+  document.querySelector(".lose p span").innerHTML = `"${word.join("")}"`;
+  word;
+  document.querySelector(".lives span").innerHTML = 5 - 0;
+  f.hideGameScreen();
+  UpdateDomWinningWord(word, inputs);
+  filterGuessedLetters(word, inputs);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".guess").addEventListener("click", guessLetter);
+  document.querySelector(".restart").addEventListener("click", startGame);
+  startGame();
+});
+
+module.exports = functions;
