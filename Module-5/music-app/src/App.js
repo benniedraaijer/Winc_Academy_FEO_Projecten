279325a39254a62899989f39ddf941fb988c6c1d
Bennie Draaijer <bennie007@live.nl>
2020-06-30 10:48:20 +0200

    Update App.js

--- Module-5/music-app/src/App.js
+++ Module-5/music-app/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./css/App.css";
-import MusicLibrary from "./components/MusicLibrary";
+import { MusicProvider } from "./contexts/MusicContext";
+import { MusicLibrary } from "./components/MusicLibrary";
 import SongInput from "./components/SongInput";
 import MusicGenres from "./components/MusicGenres.js";
 
 export default function App() {
   return (
-    <>
+    <MusicProvider>
       <SongInput />
-       <MusicGenres /> 
+      <MusicGenres />
       <MusicLibrary />
-    </>
+    </MusicProvider>
   );
 }
